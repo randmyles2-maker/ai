fb018584b159d7685436d4da8747e570c4d0b0a9
f1ways093 <randmyles2@gmail.com>
2026-04-12 03:58:32 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const cacheName = 'tro-v2';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(['./', './index.html', './index.css', './index.js', './manifest.json'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
